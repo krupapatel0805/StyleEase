1c777026931f1b88d0b542f1d26c1f27d7ab87a3
megh <shahmegh1898@gmail.com>
2024-01-11 01:46:51 +0500

    Initial Commit

---
+++ StyleEaseApp/StyleEaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleEaseAppApp.swift
+//  StyleEaseApp
+//
+//  Created by Macbook on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StyleEaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
